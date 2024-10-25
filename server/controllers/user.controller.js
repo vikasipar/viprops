2aeb7b92c767b4d3fcc2d2952f2bfb1c541415f5
vikasipar <vickyipar01@gmail.com>
2024-10-25 20:35:35 +0530

    create user model & signup api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+    res.send({
+        message: "test api success!",
+        status: 200
+    });
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// db connection
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Connected to MongoDB!");
@@ -12,8 +15,15 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
+
+
+// routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
